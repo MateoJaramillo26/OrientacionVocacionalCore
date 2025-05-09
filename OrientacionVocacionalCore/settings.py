52933072328979e87d8221d82e10d803b0b77218
MateoJaramillo <mateo.jaramillo.costa@udla.edu.ec>
2025-05-08 23:53:01 -0500

    Creacion de los hosts permitidos incluyendo vercel para poder realizar el deploy

--- OrientacionVocacionalCore/settings.py
+++ OrientacionVocacionalCore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hi-vj95xck2xq_t)zq@@n*=fusxiup2&vf+2!^hh=iym)cj))h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
