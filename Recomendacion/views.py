1fdd4904660dff06b999362cbe341b95da09854d
MateoJaramillo <mateo.jaramillo.costa@udla.edu.ec>
2025-06-03 18:33:18 -0500

    refactorizacion del codigo para tener metodos unicos en el calculo de recomendacion

--- Recomendacion/views.py
+++ Recomendacion/views.py
@@ -6,33 +6,41 @@ from collections import defaultdict
 from Academico.models import Calificacion, Clase, Facultad, Empleabilidad, InvestigacionYDescubrimiento
 # Create your views here.
 
-@login_required
-def RecomendarCarrera(request):
-    user = request.user
-    calificaciones = Calificacion.objects.filter(estudiante=user).select_related('clase')
-    facultad_notas = defaultdict(list)
-    for cal in calificaciones:
-        for facultad in cal.clase.facultad_set.all():
-            facultad_notas[facultad].append(cal.nota)
-    promedio_por_facultad = {facultad: sum(notas)/len(notas) for facultad, notas in facultad_notas.items()}
+def obtenerPromedioPorFacultad(calificaciones):
+    facultadNotas = defaultdict(list)
+    for calificacion in calificaciones:
+        for facultad in calificacion.clase.facultad_set.all():
+            facultadNotas[facultad].append(calificacion.nota)
+    return {facultad: sum(notas)/len(notas) for facultad, notas in facultadNotas.items()}
 
-    # Encontrar la(s) mejor(es) nota(s)
-    if promedio_por_facultad:
-        max_nota = max(promedio_por_facultad.values())
-        mejores_facultades = [fac for fac, nota in promedio_por_facultad.items() if nota == max_nota]
-    else:
-        mejores_facultades = []
+def obtenerMejorFacultad(promedioPorFacultad):
+    if not promedioPorFacultad:
+        return []
+    maxNota = max(promedioPorFacultad.values())
+    return [facultad for facultad, nota in promedioPorFacultad.items() if nota == maxNota]
 
+def calcularRanking(facultades):
     ranking = []
-    for facultad in mejores_facultades:
-        for uni in facultad.universidad_set.all():
-            empleabilidad = Empleabilidad.objects.filter(facultad=facultad, universidad=uni).first()
-            investigacion = InvestigacionYDescubrimiento.objects.filter(facultad=facultad, universidad=uni).first()
+    for facultad in facultades:
+        for universidad in facultad.universidad_set.all():
+            empleabilidad = Empleabilidad.objects.filter(facultad=facultad, universidad=universidad).first()
+            investigacion = InvestigacionYDescubrimiento.objects.filter(facultad=facultad, universidad=universidad).first()
             score = 0
             if empleabilidad:
                 score += empleabilidad.empleabilidad
             if investigacion:
-                score += investigacion.citacionesPorEscrito + investigacion.citacionesPorInvestigacion + investigacion.reputacionAcademica
-            ranking.append({'universidad': uni, 'facultad': facultad, 'score': score})
+                valorPromedioInvestigacion = (investigacion.citacionesPorEscrito + investigacion.citacionesPorInvestigacion + investigacion.reputacionAcademica)/3
+                score += valorPromedioInvestigacion
+                score = score/2
+            ranking.append({'universidad': universidad, 'facultad': facultad, 'score': score})
     ranking.sort(key=lambda x: x['score'], reverse=True)
-    return render(request, 'Core/recomendarCarrera.html', {'promedio_por_facultad': promedio_por_facultad, 'ranking': ranking})
+    return ranking
+            
+@login_required
+def RecomendarCarrera(request):
+    user = request.user
+    calificaiones = Calificacion.objects.filter(estudiante=user).select_related('clase')
+    promedioPorFacultad = obtenerPromedioPorFacultad(calificaiones)
+    mejorFacultad = obtenerMejorFacultad(promedioPorFacultad)
+    ranking = calcularRanking(mejorFacultad)
+    return render(request, 'Core/recomendarCarrera.html', {'promedioPorFacultad': promedioPorFacultad,'ranking': ranking})
